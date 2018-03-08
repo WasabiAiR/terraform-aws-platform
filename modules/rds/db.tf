6ac3d8c8fe36d6a8aa8ab62ec74046dcc6f9f967
jasonsattler <jason.sattler@graymeta.com>
2018-03-08 14:26:54 -0700

    Disabling auto_minor_version_upgrade

--- modules/rds/db.tf
+++ modules/rds/db.tf
@@ -1,15 +1,16 @@
 resource "aws_db_instance" "default" {
-  allocated_storage         = "${var.allocated_storage}"
-  storage_type              = "gp2"
-  engine                    = "postgres"
-  engine_version            = "9.6.6"
-  instance_class            = "${var.db_instance_size}"
-  name                      = "graymeta"
-  username                  = "${var.db_username}"
-  password                  = "${var.db_password}"
-  db_subnet_group_name      = "${aws_db_subnet_group.default.id}"
-  final_snapshot_identifier = "GrayMetaPlatform-${var.platform_instance_id}-final"
-  vpc_security_group_ids    = ["${aws_security_group.rds.id}"]
+  allocated_storage           = "${var.allocated_storage}"
+  storage_type                = "gp2"
+  engine                      = "postgres"
+  engine_version              = "9.6.6"
+  auto_minor_version_upgrade  = "false"
+  instance_class              = "${var.db_instance_size}"
+  name                        = "graymeta"
+  username                    = "${var.db_username}"
+  password                    = "${var.db_password}"
+  db_subnet_group_name        = "${aws_db_subnet_group.default.id}"
+  final_snapshot_identifier   = "GrayMetaPlatform-${var.platform_instance_id}-final"
+  vpc_security_group_ids      = ["${aws_security_group.rds.id}"]
 
   tags {
     Name               = "GrayMetaPlatform-${var.platform_instance_id}-RDS"
