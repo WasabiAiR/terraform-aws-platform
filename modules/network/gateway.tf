9fe5f74ea1c41a4e90bebf71962aa0420f6332e0
jasonsattler <jason.sattler@graymeta.com>
2018-09-05 22:04:10 -0600

    Faces Network and NAT change
    Adding two faces subnets.
    Changing the NAT configuration to have 1 NAT in each AZ for redundancy.  The only thing I found was that services/security_groups in the ALB Security Group.
    
    Also Fixing formating

--- modules/network/gateway.tf
+++ modules/network/gateway.tf
@@ -8,33 +8,33 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_eip" "nat_gateway_ecs" {
+resource "aws_eip" "nat_gateway_az1" {
   vpc = true
 }
 
-resource "aws_eip" "nat_gateway_services" {
+resource "aws_eip" "nat_gateway_az2" {
   vpc = true
 }
 
-resource "aws_nat_gateway" "ecs" {
-  allocation_id = "${aws_eip.nat_gateway_ecs.id}"
+resource "aws_nat_gateway" "az1" {
+  allocation_id = "${aws_eip.nat_gateway_az1.id}"
   subnet_id     = "${aws_subnet.public_1.id}"
   depends_on    = ["aws_internet_gateway.main"]
 
   tags {
-    Name               = "GrayMetaPlatform-${var.platform_instance_id}-ECS"
+    Name               = "GrayMetaPlatform-${var.platform_instance_id}-az1"
     Application        = "GrayMetaPlatform"
     PlatformInstanceID = "${var.platform_instance_id}"
   }
 }
 
-resource "aws_nat_gateway" "services" {
-  allocation_id = "${aws_eip.nat_gateway_services.id}"
-  subnet_id     = "${aws_subnet.public_1.id}"
+resource "aws_nat_gateway" "az2" {
+  allocation_id = "${aws_eip.nat_gateway_az2.id}"
+  subnet_id     = "${aws_subnet.public_2.id}"
   depends_on    = ["aws_internet_gateway.main"]
 
   tags {
-    Name               = "GrayMetaPlatform-${var.platform_instance_id}-Services"
+    Name               = "GrayMetaPlatform-${var.platform_instance_id}-az2"
     Application        = "GrayMetaPlatform"
     PlatformInstanceID = "${var.platform_instance_id}"
   }
@@ -44,5 +44,5 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = "${aws_vpc.main.id}"
   vpc_endpoint_type = "Gateway"
   service_name      = "com.amazonaws.${var.region}.s3"
-  route_table_ids   = ["${aws_route_table.ecs.id}","${aws_route_table.services.id}"]
+  route_table_ids   = ["${aws_route_table.az1.id}", "${aws_route_table.az2.id}"]
 }
