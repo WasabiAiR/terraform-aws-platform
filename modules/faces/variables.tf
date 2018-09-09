fbfcba65ca527d811d7b461b02a979d890ba4973
jasonsattler <jason.sattler@graymeta.com>
2018-09-09 11:04:27 -0600

    little cleanup

--- modules/faces/variables.tf
+++ modules/faces/variables.tf
@@ -1,6 +1,19 @@
-variable "platform_instance_id" {
+variable "faces_amis" {
+  type        = "map"
+  description = "map of region to ami for faces nodes"
+
+  default = {
+    "us-east-1"      = ""
+    "us-east-2"      = ""
+    "us-west-2"      = "ami-0b4943b7d0bd643b3"
+    "ap-southeast-2" = ""
+    "eu-west-1"      = ""
+  }
+}
+
+variable "faces_iam_role_name" {
   type        = "string"
-  description = "A human-readable string for this instance of the GrayMeta Platform"
+  description = "The name of the IAM role that will be applied to faces roles. Must be created by the facesiam module"
 }
 
 variable "faces_instance_type" {
@@ -21,12 +34,6 @@ variable "faces_min_cluster_size" {
   default     = "2"
 }
 
-variable "faces_volume_size" {
-  type        = "string"
-  description = "The OS disk size for Faces Server"
-  default     = "50"
-}
-
 variable "faces_subnet_id_1" {
   type        = "string"
   description = "The first subnet ID to use to deploy the faces cluster into. Needs to be in a different AZ than faces_subnet_id_2"
@@ -43,12 +50,28 @@ variable "faces_user_init" {
   default     = ""
 }
 
-variable "faces_iam_role_name" {
+variable "faces_volume_size" {
   type        = "string"
-  description = "The name of the IAM role that will be applied to faces roles. Must be created by the facesiam module"
+  description = "The OS disk size for Faces Server"
+  default     = "50"
 }
 
-# RDS
+variable "key_name" {
+  type        = "string"
+  description = "The name of the SSH key to use"
+}
+
+variable "log_retention" {
+  type        = "string"
+  description = "Optional. The log retention for cloudwatch logs.  Default 7 days"
+  default     = "7"
+}
+
+variable "platform_instance_id" {
+  type        = "string"
+  description = "A human-readable string for this instance of the GrayMeta Platform"
+}
+
 variable "rds_allocated_storage" {
   type    = "string"
   default = "50"
@@ -59,14 +82,20 @@ variable "rds_db_instance_size" {
   default = "db.t2.small"
 }
 
+variable "rds_db_password" {
+  type    = "string"
+  default = "gmchangeme"
+}
+
 variable "rds_db_username" {
   type    = "string"
   default = "gmuser"
 }
 
-variable "rds_db_password" {
-  type    = "string"
-  default = "gmchangeme"
+variable "rds_snapshot" {
+  type        = "string"
+  description = "(Optional) Specify a snapshot to use on db create.  For initial install this should be empty string.  After the initial create it is recommended to set this to final."
+  default     = ""
 }
 
 variable "rds_subnet_id_1" {
@@ -79,36 +108,12 @@ variable "rds_subnet_id_2" {
   description = "The second subnet ID to use to deploy the RDS database into. Needs to be in a different AZ than rds_subnet_id_1"
 }
 
-variable "key_name" {
-  type        = "string"
-  description = "The name of the SSH key to use"
-}
-
-variable "ssh_cidr_blocks" {
-  type        = "string"
-  description = "Comma delimited list of cidr blocks from which to allow access via SSH"
-}
-
 variable "services_ecs_cidrs" {
   type        = "list"
   description = "List of services and ecs subnet ids"
 }
 
-variable "log_retention" {
+variable "ssh_cidr_blocks" {
   type        = "string"
-  description = "Optional. The log retention for cloudwatch logs.  Default 7 days"
-  default     = "7"
-}
-
-variable "faces_amis" {
-  type        = "map"
-  description = "map of region to ami for faces nodes"
-
-  default = {
-    "us-east-1"      = ""
-    "us-east-2"      = ""
-    "us-west-2"      = "ami-0b4943b7d0bd643b3"
-    "ap-southeast-2" = ""
-    "eu-west-1"      = ""
-  }
+  description = "Comma delimited list of cidr blocks from which to allow access via SSH"
 }
