15dc78e0f23d6311dd3aeaa5a90b2ff202249a10
jasonsattler <jason.sattler@graymeta.com>
2018-09-09 14:24:42 -0600

    Add Cloudwatch for Faces and cleanup
    Added Cloudwatch for faces containers.  Cleanup some formating.

--- modules/faces/auto_scaling.tf
+++ modules/faces/auto_scaling.tf
@@ -70,14 +70,11 @@ EOF
 }
 
 resource "aws_launch_configuration" "launch_config_faces" {
-  name_prefix          = "GrayMetaPlatform-${var.platform_instance_id}-Faces-"
+  iam_instance_profile = "${aws_iam_instance_profile.iam_instance_profile_faces.name}"
   image_id             = "${lookup(var.faces_amis, data.aws_region.current.name)}"
   instance_type        = "${var.faces_instance_type}"
-  iam_instance_profile = "${aws_iam_instance_profile.iam_instance_profile_faces.name}"
   key_name             = "${var.key_name}"
-  security_groups      = ["${aws_security_group.faces.id}"]
-  user_data_base64     = "${data.template_cloudinit_config.config.rendered}"
-  depends_on           = ["aws_cloudwatch_log_group.faces"]
+  name_prefix          = "GrayMetaPlatform-${var.platform_instance_id}-Faces-"
 
   root_block_device {
     volume_type           = "gp2"
@@ -85,14 +82,19 @@ resource "aws_launch_configuration" "launch_config_faces" {
     delete_on_termination = true
   }
 
+  security_groups  = ["${aws_security_group.faces.id}"]
+  user_data_base64 = "${data.template_cloudinit_config.config.rendered}"
+
   lifecycle {
     create_before_destroy = true
   }
+
+  depends_on = ["aws_cloudwatch_log_group.faces"]
 }
 
 data "template_cloudinit_config" "config" {
-  gzip          = true
   base64_encode = true
+  gzip          = true
 
   part {
     content_type = "text/cloud-config"
@@ -110,32 +112,34 @@ data "template_file" "userdata" {
   template = "${file("${path.module}/userdata.tpl")}"
 
   vars {
+    dataversion      = "1"
+    log_group        = "${aws_cloudwatch_log_group.faces.name}"
+    postgresdb       = "faces"
     postgresendpoint = "${element(split(":", "${aws_db_instance.rds.endpoint}"), 0)}"
+    postgrespass     = "${var.rds_db_password}"
     postgresport     = 5432
     postgresuser     = "${var.rds_db_username}"
-    postgrespass     = "${var.rds_db_password}"
-    postgresdb       = "faces"
-    dataversion      = "1"
   }
 }
 
 resource "aws_autoscaling_policy" "scale_up" {
+  adjustment_type        = "ChangeInCapacity"
+  autoscaling_group_name = "${aws_cloudformation_stack.faces_asg.outputs["AsgName"]}"
+  cooldown               = 60
   name                   = "GrayMetaPlatform-${var.platform_instance_id}-Faces-scale-up"
   scaling_adjustment     = 1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 60
-  autoscaling_group_name = "${aws_cloudformation_stack.faces_asg.outputs["AsgName"]}"
 }
 
 resource "aws_autoscaling_policy" "scale_down" {
+  adjustment_type        = "ChangeInCapacity"
+  autoscaling_group_name = "${aws_cloudformation_stack.faces_asg.outputs["AsgName"]}"
+  cooldown               = 180
   name                   = "GrayMetaPlatform-${var.platform_instance_id}-Faces-scale-down"
   scaling_adjustment     = -1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 180
-  autoscaling_group_name = "${aws_cloudformation_stack.faces_asg.outputs["AsgName"]}"
 }
 
 resource "aws_cloudwatch_metric_alarm" "scale_up" {
+  alarm_actions       = ["${aws_autoscaling_policy.scale_up.arn}"]
   alarm_name          = "GrayMetaPlatform-${var.platform_instance_id}-Faces-scale-up"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "1"
@@ -144,7 +148,6 @@ resource "aws_cloudwatch_metric_alarm" "scale_up" {
   period              = "60"
   statistic           = "Average"
   threshold           = "80"
-  alarm_actions       = ["${aws_autoscaling_policy.scale_up.arn}"]
 
   dimensions {
     AutoScalingGroupName = "${aws_cloudformation_stack.faces_asg.outputs["AsgName"]}"
@@ -152,6 +155,7 @@ resource "aws_cloudwatch_metric_alarm" "scale_up" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "scale_down" {
+  alarm_actions       = ["${aws_autoscaling_policy.scale_down.arn}"]
   alarm_name          = "GrayMetaPlatform-${var.platform_instance_id}-Faces-scale-down"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "2"
@@ -160,7 +164,6 @@ resource "aws_cloudwatch_metric_alarm" "scale_down" {
   period              = "300"
   statistic           = "Average"
   threshold           = "50"
-  alarm_actions       = ["${aws_autoscaling_policy.scale_down.arn}"]
 
   dimensions {
     AutoScalingGroupName = "${aws_cloudformation_stack.faces_asg.outputs["AsgName"]}"
--- modules/faces/iam.tf
+++ modules/faces/iam.tf
@@ -1,13 +1,13 @@
 resource "aws_iam_policy" "iam_policy" {
-  name        = "GrayMetaPlatform-${var.platform_instance_id}-Faces-Policy"
   description = "GrayMeta Platform Faces Nodes privileges"
+  name        = "GrayMetaPlatform-${var.platform_instance_id}-Faces-Policy"
   policy      = "${data.template_file.policy_faces.rendered}"
 }
 
 resource "aws_iam_policy_attachment" "iam_policy_attachment" {
   name       = "${aws_iam_policy.iam_policy.name}"
-  roles      = ["${var.faces_iam_role_name}"]
   policy_arn = "${aws_iam_policy.iam_policy.arn}"
+  roles      = ["${var.faces_iam_role_name}"]
 }
 
 resource "aws_iam_instance_profile" "iam_instance_profile_faces" {
