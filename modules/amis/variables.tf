670508826472c2b2043474a0db580eb25d2e6865
Jason Sattler <jason.sattler@graymeta.com>
2019-02-15 10:38:21 -0700

    New-Build-v0.1.6

--- modules/amis/variables.tf
+++ modules/amis/variables.tf
@@ -3,11 +3,11 @@ variable "ecs_amis" {
   description = "map of region to ami for ecs nodes"
 
   default = {
-    "ap-southeast-2" = "ami-0c200ee6ed0c39907"
-    "eu-west-1"      = "ami-03d443b9700eb0c5c"
-    "us-east-1"      = "ami-03279f49db725b2aa"
-    "us-east-2"      = "ami-0795f95e0574b17c5"
-    "us-west-2"      = "ami-0a231fb8ef2df0d92"
+    "ap-southeast-2" = "ami-066c3c54d2b98b670"
+    "eu-west-1"      = "ami-059cd5de8b205d94a"
+    "us-east-1"      = "ami-0a9e571d6cb7d1e45"
+    "us-east-2"      = "ami-0650a910f15be2819"
+    "us-west-2"      = "ami-043b297a59fa12e49"
   }
 }
 
@@ -16,11 +16,11 @@ variable "mlservices_amis" {
   description = "map of region to ami for mlservices nodes"
 
   default = {
-    "ap-southeast-2" = "ami-0612899aaab2a9f45"
-    "eu-west-1"      = "ami-0d519ea25a5d2914d"
-    "us-east-1"      = "ami-0bdf31d1f725256ed"
-    "us-east-2"      = "ami-0b1dfd6b0761f4505"
-    "us-west-2"      = "ami-0534453055684be0d"
+    "ap-southeast-2" = "ami-07139076a8c77a216"
+    "eu-west-1"      = "ami-0f10b3b9b9f77aee9"
+    "us-east-1"      = "ami-08f822e1bdc7cb46b"
+    "us-east-2"      = "ami-0e1ff7f55a515009d"
+    "us-west-2"      = "ami-07b0da69c39c2cfbb"
   }
 }
 
@@ -29,11 +29,11 @@ variable "proxy_amis" {
   description = "map of region to ami for proxy nodes"
 
   default = {
-    "ap-southeast-2" = "ami-081c3bca5bb0fb280"
-    "eu-west-1"      = "ami-09d0344514bef6484"
-    "us-east-1"      = "ami-0b4fa399234c5ab0b"
-    "us-east-2"      = "ami-07586ac64d109340a"
-    "us-west-2"      = "ami-017379caf2e25ac14"
+    "ap-southeast-2" = "ami-04ced742a0bac12d8"
+    "eu-west-1"      = "ami-0ff09655b596b9c79"
+    "us-east-1"      = "ami-0b968435bef896c03"
+    "us-east-2"      = "ami-089b370abbb79d959"
+    "us-west-2"      = "ami-0d051bfbcc2cc328c"
   }
 }
 
@@ -42,10 +42,10 @@ variable "services_amis" {
   description = "map of region to ami for services nodes"
 
   default = {
-    "ap-southeast-2" = "ami-080dfeddac76f3ad7"
-    "eu-west-1"      = "ami-0123f9b2f35c0ee5a"
-    "us-east-1"      = "ami-0933c032b60d16c39"
-    "us-east-2"      = "ami-0dc50bafb082d64f0"
-    "us-west-2"      = "ami-0f9c9d94b62145028"
+    "ap-southeast-2" = "ami-04ae3294c445400b8"
+    "eu-west-1"      = "ami-0454c6b2f311463dc"
+    "us-east-1"      = "ami-0beac6d094fdc2bbb"
+    "us-east-2"      = "ami-0400284db40e5e310"
+    "us-west-2"      = "ami-05f7c2bbf2cfc977a"
   }
 }
