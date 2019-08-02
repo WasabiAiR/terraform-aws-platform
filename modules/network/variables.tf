2dd90065ae9695cee229650c21eaa8d58e77979e
Jason Sattler <jason.sattler@graymeta.com>
2019-08-02 09:46:33 -0600

    adding cognitive

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -216,6 +216,7 @@ variable "safelist" {
     ".graymeta.com",
     ".kairos.com",
     ".logograb.com",
+    ".cognitive.microsoft.com",
     ".speech.microsoft.com",
     ".picpurify.com",
     ".platform.bing.com",
